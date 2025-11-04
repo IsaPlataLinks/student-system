b2ccc37e9a7376869aa105948a233caec510c5cf
IsaPlataLinks <isabella.delaplata@hotmail.com>
2025-11-04 20:10:32 -0300

    atualizando

--- app.py
+++ app.py
@@ -591,4 +591,4 @@ if __name__ == '__main__':
         db.create_all()
         criar_usuario_admin()
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
